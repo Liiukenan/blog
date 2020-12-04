86968f208b92ae5c29e29ab20d7277c4026ffadd
liukenan <liukenan@meetstarinc.com>
2020-12-04 19:40:59 +0800

    路径修改

--- config/servicePath.js
+++ config/servicePath.js
@@ -1,4 +1,4 @@
-let ipUrl = "http://127.0.0.1:7001"; //接口
+let ipUrl = "http://188.131.143.196:7001"; //接口
 let servicePath = {
   getArticleList: ipUrl + "/getArticleList", //首页接口
   getArticleById: ipUrl + "/getArticleById/", //详细页接口
